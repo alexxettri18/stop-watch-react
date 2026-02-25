5153adbe90563028d0bfec6093fbded36c1cf196
alexxettri18 <bstalexxtri22@gmail.com>
2026-02-25 09:47:54 +0545

    arexkkusu

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./App.css"
 
-import Stopwatch from './Stopwatch07.jsx'
+import Stopwatch from './Stopwatch07'
 
 
 createRoot(document.getElementById('root')).render(
